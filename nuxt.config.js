8588dcad6abbd7ac1cc84d442b276140461b9ca1
rokealvo <rokealvo@gmail.com>
2019-11-09 18:33:23 +0700

    add langAttr to html

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,7 @@ export default {
    */
   head: {
     title: process.env.npm_package_name || '',
+    htmlAttrs: { lang: 'ru' },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
