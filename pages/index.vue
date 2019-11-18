d500885136316093b8e2d490c4933b2f3bbee80c
rokealvo <rokealvo@gmail.com>
2019-11-18 23:00:22 +0700

    change  npm build

---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+  .page
+    Header(:city='this.$route.params.city')
+    Section1(class="page__section1")
+    Section2
+    Section3
+    Section4
+    Section5
+    Section6
+    Footer
+</template>
+
+<script>
+import Section1 from '@/components/home/Section1.vue'
+import Header from '~/components/Header'
+import Section2 from '@/components/home/Section2.vue'
+import Section3 from '@/components/home/Section3.vue'
+import Section4 from '@/components/home/Section4.vue'
+import Section5 from '@/components/home/Section5.vue'
+import Section6 from '@/components/home/Section6.vue'
+
+export default {
+  components: {
+    Header,
+    Section1,
+    Section2,
+    Section3,
+    Section4,
+    Section5,
+    Section6
+  }
+}
+</script>
+
+<style>
+.page {
+  overflow-x: hidden;
+  max-width: 100vw;
+}
+</style>
