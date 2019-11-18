70a7f27bd0269e1d959e20891e644b6e7c3e1f4b
rokealvo <rokealvo@gmail.com>
2019-11-18 23:23:29 +0700

    deploy to devserver

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   .page
-    Header(city='Новосибирск')
+    Header(:city='city')
     Section1(class="page__section1")
     Section2
     Section3
@@ -28,6 +28,11 @@ export default {
     Section4,
     Section5,
     Section6
+  },
+  computed: {
+    city() {
+      return this.$route.property || 'Новосибирск'
+    }
   }
 }
 </script>
